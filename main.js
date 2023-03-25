d71b69e295481055b13f55b3e9a0365df66629ee
Toevlucht <m.wezen@gmail.com>
2023-03-25 18:45:40 +0100

    Increase button font-weight & add JS comments

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
 const gridContainer = document.querySelector('.grid-container');
 let isMouseDown = false;
 let lastSelectedSize = 16;
+let size = 16;
 let currentColor = "black";
 
-//create a new grid based on variable 'numbSquares'
-function createGrid(numbSquares) {
-    lastSelectedSize = numbSquares;
 
-    for (let i = 0; i < numbSquares * numbSquares; i++) {
+//create a new grid based on variable 'numbSquares'
+function createGrid(size) {
+    lastSelectedSize = size;
+
+    for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
         square.classList.add('square')
-        gridContainer.style.gridTemplateColumns = `repeat(${numbSquares}, 1fr)`;
-        gridContainer.style.gridTemplateRows = `repeat(${numbSquares}, 1fr)`;
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
         gridContainer.appendChild(square);
 
+        //if mousedown, draw
         square.addEventListener('mousedown', () => {
             isMouseDown = true;
             draw(square);
         });
 
+        //also draw in case of mouseover, if isMouseDown is true
         square.addEventListener('mouseover', function() {
             if (isMouseDown) {
                 draw(square);
             }
         });
 
+        //stop drawing when releasing 'click'
         square.addEventListener('mouseup', function() {
             isMouseDown = false;
         });
@@ -42,33 +47,30 @@ function draw(square) {
     }
 };
 
+//delete grid
 function deleteGrid () {
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
 }
 
+
+const rangeDisplay = document.querySelector('#range-display')
+rangeDisplay.textContent = `${size} x ${size}`
+
 //get new grid-size number
 const grid = document.querySelector('#new-grid')
-grid.addEventListener('click', function() {
-    let numbSquares;
 
-    while (true) {
-        let askSquares = prompt('Enter the number of squares per side (max. 100)');
-        numbSquares = parseInt(askSquares);
-
-        if (!isNaN(numbSquares) && (numbSquares <= 100)) {
-            break;
-        } 
-        
-        alert ("invalid input, please enter a number (max. 100)");
-        return numbSquares;  
-    }
-
-    deleteGrid();
-    createGrid(numbSquares);
-
+grid.addEventListener('input', () => {
+    const gridSize = grid.value;
+    updateGrid(gridSize);
 });
+
+function updateGrid(size) {
+    rangeDisplay.textContent = `${size} x ${size}`
+    deleteGrid();
+    createGrid(size);
+}
 
 //reset the sketch and set to last selected amount of pixels
 const reset = document.querySelector('#reset') 
@@ -81,11 +83,11 @@ reset.addEventListener('click', function() {
 //generate a random rgb color
 const randomColor = document.querySelector('#random-color');
 randomColor.addEventListener('click', () => {
-    if (randomColor.textContent === "Random color") {
+    if (randomColor.textContent === "Rainbow stylus") {
         randomColor.textContent = "Black stylus";
         currentColor = "random";
     } else {
-        randomColor.textContent = "Random color";
+        randomColor.textContent = "Rainbow stylus";
         currentColor = "black";
     }
 
